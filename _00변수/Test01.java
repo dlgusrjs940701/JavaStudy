388e818703122fda0c5d147816fe6628a69880e0
dlgusrjs <dlgusrjs07@naver.com>
2024-08-06 10:50:18 +0900

    00변수 미션완료

--- _00변수/Test01.java
+++ _00변수/Test01.java
@@ -4,7 +4,16 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// 정수 변수 선언
+		int a = 10;
+		byte aa =10;
+		System.out.println(a);
+		System.out.println(aa);
+		// 소스 실행하는 단축키 : 컨트롤 + f11
+		
+		// 문자열 변수 선언
+		String s = "Hyun Gun";
+		System.out.println(s);
 	}
 
 }
