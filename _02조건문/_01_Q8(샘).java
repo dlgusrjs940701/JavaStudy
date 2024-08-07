3fb7e5ae7ac132e15d30ec2b59a4f361601864eb
dlgusrjs <dlgusrjs07@naver.com>
2024-08-07 17:21:31 +0900

    자바의 조건

---
+++ _02조건문/_01_Q8(샘).java
@@ -0,0 +1,22 @@
+package _02if;
+
+public class _01_Q8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] x = {6,5,9};
+		int[] y = {4,10,6};
+		double k = 3.0/2.0;		
+		if(k == (double)x[0]/y[0]) {
+			System.out.println(0);
+		}
+		if(k == (double)x[1]/y[1]) {
+			System.out.println(1);
+		}
+		if(k == (double)x[2]/y[2]) {
+			System.out.println(2);
+		}
+
+	}
+
+}
