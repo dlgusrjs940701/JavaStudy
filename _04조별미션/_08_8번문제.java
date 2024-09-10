6f19a803f32b98ff56a63f1abc89da6408a99ab0
dlgusrjs940701 <dlgusrjs07@naver.com>
2024-09-10 17:15:52 +0900

    자바 2차원배열

--- _04조별미션/_08_8번문제.java
+++ _04조별미션/_08_8번문제.java
@@ -8,16 +8,16 @@ public class _08_8번문제 {
 		// 0번 인덱스가 가장 큰 값이라 정의하고 1번부터 마지막까지 순회하면서
 		// 가장 큰 값이 나오면 가장 큰 값을 갱신한다.
 		// 위와 같은 방식으로 가장 큰 수를 구하시오.
-		int[] a = {34,2,35,8,31,45};
-		
-		for(int i=1;i<a.length-1;i++) {
-			if(a[i]>a[i+1]) {
-				a[i]=a[i]+a[i+1];
-				a[i+1]=a[i]-a[i+1];
-				a[i]=a[i]-a[i+1];
+		int[] a = { 34, 2, 35, 8, 31, 45 };
+
+		for (int i = 1; i < a.length - 1; i++) {
+			if (a[i] > a[i + 1]) {
+				a[i] = a[i] + a[i + 1];
+				a[i + 1] = a[i] - a[i + 1];
+				a[i] = a[i] - a[i + 1];
 			}
-			if(a[a.length-1]>a[0]) {
-				a[0]=a[a.length-1];
+			if (a[a.length - 1] > a[0]) {
+				a[0] = a[a.length - 1];
 			}
 		}
 		System.out.println(a[0]);
